db916ce4d6502de2201207aa2d46706179511ccc
Luciano Figueroa <lumusitech@gmail.com>
2024-09-25 16:48:38 -0300

    feat: add base issue and issues list pages

---
+++ src/app/modules/issues/pages/issue-page/issue-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-page.component.html',
+})
+export default class IssuePageComponent {}
---
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issues-list-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './issues-list-page.component.html',
+})
+export default class IssuesListPageComponent {}
