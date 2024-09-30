26ca7a6857e0690cb6ab704ab3328611a2d996a2
Luciano Figueroa <lumusitech@gmail.com>
2024-09-29 21:13:09 -0300

    feat: add base link to mock issue /issue/123

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'issues-list-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './issues-list-page.component.html',
 })
 export default class IssuesListPageComponent {}
