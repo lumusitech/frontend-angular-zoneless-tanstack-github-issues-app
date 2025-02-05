97da141c660196276c53360fac3da45972c73151
Luciano Figueroa <lumusitech@gmail.com>
2025-02-04 21:40:29 -0300

    refactor: delete final space into url

--- src/app/modules/issues/actions/get-issue-by-number.action.ts
+++ src/app/modules/issues/actions/get-issue-by-number.action.ts
@@ -13,19 +13,19 @@ export const getIssueByNumber = async (
   await sleep(1500);
 
   try {
-    const resp = await fetch(`${BASE_URL}/issues/${issueNumber} `, {
+    const resp = await fetch(`${BASE_URL}/issues/${issueNumber}`, {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
       },
     });
 
-    if (!resp.ok) throw new Error("Can't load issue ");
+    if (!resp.ok) throw new Error("Can't load issue");
 
     const issue: GithubIssue = await resp.json();
     console.log({ issue });
 
     return issue;
   } catch (error) {
-    throw new Error("Can't load issue");
+    throw `Can't load issue ${issueNumber}`;
   }
 };
