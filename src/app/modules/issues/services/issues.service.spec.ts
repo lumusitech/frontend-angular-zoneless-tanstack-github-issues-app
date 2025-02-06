9249f1bd2f93f462d41f67b61053112593fa5c8f
Luciano Figueroa <lumusitech@gmail.com>
2025-02-06 00:38:59 -0300

    test: add final tests to issue service

--- src/app/modules/issues/services/issues.service.spec.ts
+++ src/app/modules/issues/services/issues.service.spec.ts
@@ -3,10 +3,11 @@ import {
   provideAngularQuery,
   QueryClient,
 } from '@tanstack/angular-query-experimental';
-import { IssueService } from './issue.service';
+import { State } from '../interfaces';
+import { IssuesService } from './issues.service';
 
 describe('IssueService', () => {
-  let service: IssueService;
+  let service: IssuesService;
   const queryClient = new QueryClient();
 
   beforeEach(() => {
@@ -17,7 +18,7 @@ describe('IssueService', () => {
       teardown: { destroyAfterEach: false },
       providers: [provideAngularQuery(queryClient)],
     });
-    service = TestBed.inject(IssueService);
+    service = TestBed.inject(IssuesService);
   });
 
   it('should be created', () => {
@@ -40,5 +41,44 @@ describe('IssueService', () => {
     expect(typeof label.name).toBe('string');
     expect(typeof label.node_id).toBe('string');
     expect(typeof label.url).toBe('string');
+  });
+
+  it('should set selected state - OPEN, CLOSED and ALL', async () => {
+    service.showIssuesByState(State.Closed);
+    expect(service.selectedState()).toBe(State.Closed);
+
+    const { data: dataClosed } = await service.issuesQuery.refetch();
+
+    dataClosed?.forEach((issue) => {
+      expect(issue.state).toBe(State.Closed);
+    });
+
+    service.showIssuesByState(State.Open);
+    const { data: dataOpen } = await service.issuesQuery.refetch();
+
+    dataOpen?.forEach((issue) => {
+      expect(issue.state).toBe(State.Open);
+    });
+  });
+
+  it('should set selected labels', async () => {
+    service.toggleLabel('Accessibility');
+    expect(service.selectedLabels().has('Accessibility')).toBeTruthy();
+
+    service.toggleLabel('Accessibility');
+    expect(service.selectedLabels().has('Accessibility')).toBeFalsy();
+  });
+
+  it('should set selectedLabels and get issues by label', async () => {
+    const label = 'Accessibility';
+    service.toggleLabel(label);
+    expect(service.selectedLabels().has(label)).toBeTruthy();
+
+    const { data } = await service.issuesQuery.refetch();
+
+    data?.forEach((issue) => {
+      const hasLabel = issue.labels.some((l) => l.name === label);
+      expect(hasLabel).toBeTruthy();
+    });
   });
 });
